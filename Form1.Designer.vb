0a1d72249f5c0aab82885a545cfea554d80d096f
NJ Bendall <33398449+njbendall@users.noreply.github.com>
2025-03-22 22:50:29 +0000

    Added registry writing and branding

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,179 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPort = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtUrl = New System.Windows.Forms.TextBox()
+        Me.txtTopic = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.mqtt_handler_config_util.My.Resources.Resources.HisSmartHome_black
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 18)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(288, 32)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(12, 98)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(100, 20)
+        Me.txtServer.TabIndex = 1
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(12, 161)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtPort
+        '
+        Me.txtPort.Location = New System.Drawing.Point(175, 98)
+        Me.txtPort.Name = "txtPort"
+        Me.txtPort.Size = New System.Drawing.Size(100, 20)
+        Me.txtPort.TabIndex = 3
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(175, 161)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 4
+        '
+        'txtUrl
+        '
+        Me.txtUrl.Location = New System.Drawing.Point(175, 221)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(100, 20)
+        Me.txtUrl.TabIndex = 5
+        '
+        'txtTopic
+        '
+        Me.txtTopic.Location = New System.Drawing.Point(12, 221)
+        Me.txtTopic.Name = "txtTopic"
+        Me.txtTopic.Size = New System.Drawing.Size(100, 20)
+        Me.txtTopic.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "MQTT Server"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(172, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "MQTT Port"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Username"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(172, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Password"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(172, 209)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "URL"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 209)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Topic"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(105, 259)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 13
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(287, 294)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtTopic)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtPort)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "MQTT Handler Config Utility"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPort As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtUrl As TextBox
+    Friend WithEvents txtTopic As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnSave As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,41 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Win32
+Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        MsgBox("Created by NJ Bendall. https://hsho.me")
+    End Sub
 
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Try
+            Dim baseKey As RegistryKey = Registry.LocalMachine.CreateSubKey("Software\His Smart Home\MQTT-Listener")
+
+            If baseKey IsNot Nothing Then
+                baseKey.SetValue("Server", txtServer.Text, RegistryValueKind.String)
+
+                ' Port should be numeric
+                Dim portValue As Integer
+                If Integer.TryParse(txtPort.Text, portValue) Then
+                    baseKey.SetValue("Port", portValue, RegistryValueKind.DWord)
+                Else
+                    MessageBox.Show("Port must be a valid number.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    Return
+                End If
+
+                baseKey.SetValue("Username", txtUsername.Text, RegistryValueKind.String)
+                baseKey.SetValue("Password", txtPassword.Text, RegistryValueKind.String)
+                baseKey.SetValue("Topic", txtTopic.Text, RegistryValueKind.String)
+                baseKey.SetValue("UrlToOpen", txtUrl.Text, RegistryValueKind.String)
+
+                baseKey.Close()
+
+                MessageBox.Show("Settings saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Failed to open registry key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+        Catch ex As UnauthorizedAccessException
+            MessageBox.Show("Access denied. Please run as administrator.", "Permission Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            MessageBox.Show("Error saving settings: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 End Class
